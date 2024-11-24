ad6b5a9c25a0a1e30e8eee3d51bab1455074da47
Trisha <trishamachavarapu123@gmail.com>
2024-11-24 16:11:38 +0530

    Created finding_max.c

---
+++ finding_max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n,ele;
+    printf("Enter no.of elements you want");
+    scanf("%d",&n);
+    for(int i =1;i<=n;i++){
+        int temp;
+        printf("Enter %d element : ",i);
+        scanf("%d",&temp);
+        if(i==1)
+        ele=temp;
+        else if(ele<temp)
+            ele=temp;
+    }
+    printf("The largest element is : %d",ele);
+    return 0;
+}
